e84f11db0bfeb162300acba45736d021bfd210bf
Gaurang Sharma <gaurangsharma.003@gmail.com>
2024-11-29 13:43:14 +0530

    new repo for form of validation

--- src/main/java/com/springmvc/thymeleaf/model/Customer.java
+++ src/main/java/com/springmvc/thymeleaf/model/Customer.java
@@ -1,4 +1,52 @@
 package com.springmvc.thymeleaf.model;
 
+
+import jakarta.validation.constraints.*;
+
 public class Customer {
+
+    private String firstName;
+
+   @NotNull(message="is required")
+   @Size(min = 1, message = "is required")
+   private String lastName ="";
+
+      @Min(value = 0 , message="must not be greater than or equal to Zero")
+      @Max(value = 10 , message="must not be less than or equal to 10")
+      private int freePasses;
+
+      @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "ONly 5 chars/ digits")
+      private String postalCode;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
